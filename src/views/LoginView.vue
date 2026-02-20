e3f5fcfd119b0b58ec549bfb8a0eaad3dbca2b98
Itami00001 <arturpodrugov@gmail.com>
2026-02-20 12:52:28 +0300

    Шаг 3: Верстка страницы логина

---
+++ src/views/LoginView.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" lg="6">
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Вход в Автопарк</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                prepend-icon="mdi-account"
+                name="email"
+                label="Email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="mdi-lock"
+                name="password"
+                label="Password"
+                type="password"
+                v-model="password"
+                :rules="passwordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" :disabled="!valid" @click="login">Войти</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be more than 6 characters'
+      ]
+    }
+  },
+  methods: {
+    login() {
+      if (this.$refs.form.validate()) {
+        console.log('Login attempt:', this.email, this.password)
+      }
+    }
+  }
+}
+</script>
